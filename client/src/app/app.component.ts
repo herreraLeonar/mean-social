084ba48a1ef79f13861b0411ef4f02f5966dc07e
LEONAR <portabsystem@gmail.com>
2018-08-25 11:10:31 -0300

    integration angular 6
    install angular cli

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+declare var JQuery: any;
+declare var $: any;
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'client';
+}
